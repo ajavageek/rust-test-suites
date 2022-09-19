ea40c33f14f65c5b02ffde1a0b434b2ef3bee4a2
Nicolas Fränkel <nicolas@frankel.ch>
2022-09-19 15:34:44 +0200

    Change function signatures and test them

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
+#[cfg(all(feature = "unit", feature = "it"))]
+compile_error!("feature \"unit\" and feature \"it\" cannot be enabled at the same time");
+#[cfg(all(feature = "unit", feature = "prod"))]
+compile_error!("feature \"unit\" and feature \"prod\" cannot be enabled at the same time");
+#[cfg(all(feature = "it", feature = "prod"))]
+compile_error!("feature \"it\" and feature \"prod\" cannot be enabled at the same time");
+
+#[cfg(feature = "prod")]
 fn main() {
-    #[cfg(feature = "unit")]
-    let db = MockDatabase {};
-    #[cfg(feature = "it")]
-    let db = SqlitDatabase {};
-    #[cfg(feature = "prod")]
     let db = PostgreSqlDatabase {};
-    db.do_stuff();
+    println!("{}", db.do_stuff());
 }
 
 trait Database {
-    fn do_stuff(self: Self);
+    fn do_stuff(self: Self) -> &'static str;
 }
 
 #[cfg(feature = "unit")]
@@ -21,21 +24,35 @@ struct PostgreSqlDatabase {}
 
 #[cfg(feature = "unit")]
 impl Database for MockDatabase {
-    fn do_stuff(self: Self) {
-        println!("Do mock stuff");
+    fn do_stuff(self: Self) -> &'static str {
+        "Do mock stuff"
     }
 }
 
 #[cfg(feature = "it")]
 impl Database for SqlitDatabase {
-    fn do_stuff(self: Self) {
-        println!("Do stuff with SQLite");
+    fn do_stuff(self: Self) -> &'static str {
+        "Do stuff with SQLite"
     }
 }
 
 #[cfg(feature = "prod")]
 impl Database for PostgreSqlDatabase {
-    fn do_stuff(self: Self) {
-        println!("Do stuff with PostgreSQL");
+    fn do_stuff(self: Self) -> &'static str {
+        "Do stuff with PostgreSQL"
     }
+}
+
+#[test]
+#[cfg(feature = "unit")]
+fn test_unit() {
+    let db = MockDatabase {};
+    assert_eq!(db.do_stuff(), "Do mock stuff");
+}
+
+#[test]
+#[cfg(feature = "it")]
+fn test_it() {
+    let db = SqlitDatabase {};
+    assert_eq!(db.do_stuff(), "Do stuff with SQLite");
 }
